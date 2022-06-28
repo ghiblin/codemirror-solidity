27480a819c867b8385cabc86dc92ecb1f9829cc7
Alessandro Vitali <alessandro.vitali@consensys.net>
2022-06-28 12:17:43 +0200

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { EditorState } from "@codemirror/state";
+import { solidity } from "@replit/codemirror-lang-solidity";
+import { basicSetup, EditorView } from "codemirror";
+import { useEffect, useRef } from "react";
+
+const doc = `
+pragma solidity ^0.8.10;
+
+contract EtherWallet {
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    receive() external payable {}
+
+    function withdraw(uint _amount) external {
+        require(msg.sender == owner, "caller is not owner");
+        payable(msg.sender).transfer(_amount);
+    }
+
+    function getBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+}
+`
 
 function App() {
+  const editor = useRef();
+
+  useEffect(() => {
+    console.log('create new EditorView...')
+    const view = new EditorView({
+      state: EditorState.create({
+        doc,
+        extensions: [
+          basicSetup,
+          solidity
+        ]
+      }),
+      parent: editor.current
+    })
+    return () => {
+      view.destroy();
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>CodeMirror with solidity</h1>
+      <div ref={editor}></div>
     </div>
   );
 }
